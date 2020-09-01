1017113a368c16a9e4e19c903e4e717fcbd7993c
Tarciso Torres <tarcisoftorres@gmail.com>
2020-08-31 22:39:08 -0300

    Change type id to String

--- src/main/java/com/insight/desafio/entity/User.java
+++ src/main/java/com/insight/desafio/entity/User.java
@@ -13,14 +13,14 @@ public class User{
 
 	@Id @Type(type = "pg-uuid")
 	@GeneratedValue
-	private Long id;
+	private String id;
 	private String name;
 
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
