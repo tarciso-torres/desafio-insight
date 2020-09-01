56b8a28629df8065c4f1ed66c64c57a3c146b083
Tarciso Torres <tarcisoftorres@gmail.com>
2020-08-31 22:10:17 -0300

    Fix import in user entity

--- src/main/java/com/insight/desafio/entity/User.java
+++ src/main/java/com/insight/desafio/entity/User.java
@@ -3,6 +3,7 @@ package com.insight.desafio.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.Type;
 
